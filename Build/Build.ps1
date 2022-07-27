bae3e58c592e4770c0c4227b4c2da3ecd15c63bc
aj.lindner@quest.com <aj.lindner@quest.com>
2022-07-27 18:59:52 -0500

    removed Build directory

--- Build/Build.ps1
+++
@@ -1,5 +0,0 @@
-# Update Help Documentation
-Set-Location "C:\Scripts\Modules\OneIdentity-Password-Manager-OneLogin-Integration\"
-Import-Module ".\OneLoginByOneIdentity\OneLoginByOneIdentity.psd1"
-Update-MarkdownHelpModule -Path .\Docs -RefreshModulePage
-New-ExternalHelp .\Docs -OutputPath .\OneLoginByOneIdentity\en-US\ -Force
